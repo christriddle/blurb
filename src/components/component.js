253f596d789e519c4aee71e4e3fc2feb99667b6e
Elliott Thompson <elliott@moo.com>
2016-02-26 15:01:24 +0000

    fixed the exporting issue and passed the selectedCorner function to the RadioGroup class

--- src/components/component.js
+++ src/components/component.js
@@ -11,29 +11,21 @@ var ActionPanel = React.createClass({
 	},
 	selectedCorner: function(selected) {
 		this.state.selectedCorner = selected;
-		console.log(this.state.selectedCorner);
+        console.log(this.state.selectedCorner);
 	},
 	render: function() 		{	
-		return (<CornerTypeRadioGroup selectedCorner={"foo"} />);
+		return (<CornerTypeRadioGroup selectedCorner={this.selectedCorner} />);
 	}
 });
 
 var CornerTypeRadioGroup = React.createClass({
-	onSquareChange: function(e) {
-		console.log(this.props)
-		this.props.selectedCorner("square");
-	},
-	onRoundedChange: function(e) {
-		this.props.selectedCorner("rounded");
-	},
 	render: function() {
 		return (<div>
 			<p>Corner type</p>
-			<label htmlFor="square">Square</label><input onChange={ this.onSquareChange } type="radio" value="square" id="square" name="corner-group" />
-			<label htmlFor="rounded">Rounded</label><input onChange={ this.onRoundedChange } type="radio" value="rounded" id="rounded" name="corner-group" />
+			<label htmlFor="square">Square</label><input onChange={ this.props.selectedCorner.bind(null, "square") } type="radio" value="square" id="square" name="corner-group" />
+			<label htmlFor="rounded">Rounded</label><input onChange={ this.props.selectedCorner.bind(null, "rounded") } type="radio" value="rounded" id="rounded" name="corner-group" />
 		</div>);
 	}
 });
 
 module.exports = ActionPanel;
-module.exports = CornerTypeRadioGroup;
